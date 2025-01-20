7e3f2b6092cacd783ac1a7e34c5ed39033815b9b
Jewel-cse <md.jewel.cse@gmail.com>
2025-01-20 22:59:58 +0600

    project initialized

---
+++ back-end/prescription-generation-app/src/main/java/com/rana/prescription_generation_app/PrescriptionGenerationAppApplication.java
@@ -0,0 +1,13 @@
+package com.rana.prescription_generation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionGenerationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionGenerationAppApplication.class, args);
+	}
+
+}
---
+++ back-end/prescription-generation-app/src/test/java/com/rana/prescription_generation_app/PrescriptionGenerationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rana.prescription_generation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionGenerationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
